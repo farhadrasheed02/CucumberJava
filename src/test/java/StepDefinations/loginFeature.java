f7047c82b281d64ea39aa0557586389a2e607c5e
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-05-30 21:19:36 +0530

    Commit By FR

--- src/test/java/StepDefinations/loginFeature.java
+++ src/test/java/StepDefinations/loginFeature.java
@@ -9,19 +9,19 @@ public class loginFeature {
 	@Given("user is on login page")
 	public void login_page() {
 		System.out.println("In login page");
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	}
 
 	@When("user enters username and password")
 	public void username_and_password() {
 		System.out.println("In user name and password section");
-	    // Write code here that turns the phrase above into concrete actions
+	   
 	}
 
 	@And("clicks on login button")
 	public void login_button() {
 		System.out.println("On login button");
-	    // Write code here that turns the phrase above into concrete actions
+	    
 	}
 
 	@Then("user is navigated to the login page")
