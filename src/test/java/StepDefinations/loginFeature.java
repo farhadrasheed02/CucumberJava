bf1518360e3ac9739a834cf3cb76842e42be1e74
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-03 00:48:22 +0530

    Commit By FR

--- src/test/java/StepDefinations/loginFeature.java
+++ src/test/java/StepDefinations/loginFeature.java
@@ -1,11 +1,33 @@
 package StepDefinations;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class loginFeature {
+	static PrintStream stream;
+	@Then("report will be generated in file")
+	public void generate_report() {
+		 
+		File file = new File("src/test/resources/Results/Log.txt");
+		try {
+			 stream = new PrintStream(file);
+			System.out.println("From now onwards"+file.getAbsolutePath()+"will be you console");
+		//	System.out.println("==============="+file.getAbsolutePath());
+			System.setOut(stream);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
+  
 	@Given("user is on login page")
 	public void login_page() {
 		System.out.println("In login page");
@@ -28,6 +50,7 @@ public class loginFeature {
 	public void navigated_to_the_login_page() {
 		System.out.println("I am navigated to the login page wadrobe");
 	}
+	
 
 
 
