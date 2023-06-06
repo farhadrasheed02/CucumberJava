c7219cab1927b115c509e8e56d5f9a0bf91c8d70
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-06 19:41:03 +0530

    Commit By FR

---
+++ src/test/java/RESTAssured/BooksReq.java
@@ -0,0 +1,26 @@
+package RESTAssured;
+
+import org.testng.annotations.Test;
+
+import groovyjarjarasm.asm.commons.Method;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+@Test
+public class BooksReq {
+	@Test
+	public void GetBookDetails()
+	{
+		RestAssured.baseURI="https://demoqa.com/BookStore/v1/Books";
+		RequestSpecification httpRequest = RestAssured.given(); 
+		Response response = httpRequest.get();
+		System.out.println("Status received => " + response.getStatusLine()); 
+		System.out.println("Response=>" + response.prettyPrint());
+	}
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
--- src/test/java/RESTAssured/PostReq.java
+++ src/test/java/RESTAssured/PostReq.java
@@ -1,6 +1,10 @@
 package RESTAssured;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.json.simple.JSONObject;
@@ -10,42 +14,49 @@ import static io.restassured.RestAssured.given;
 
 public class PostReq {
 	@Test
-//	public void PostReqMeth()
-//	{
-//		//sending request body as String ....
+	public void GeeksforGeek()
+	{
+		RestAssured
+        .get("http://my-api.com/resource/123")
+        .then()
+        .assertThat()
+        .statusCode(200)
+        .and()
+        .contentType(ContentType.JSON)
+        .and()
+        .body("id", equalTo(123));
+	}
+	@Test
+	public void PostReqMeth()
+	{
+		//sending request body as String ....
 //		String pbody ="{\"id\":1,"
 //				+ "email\":\"george.bluth@reqres.in\","
 //				+ "\"first_name\":\"George\","
 //				+ "\"last_name\":\"Bluth\","
 //				+ "\"avatar\":\"https://reqres.in/img/faces/1-image.jpg\"}";
-//	 RestAssured.baseURI="https://reqres.in/";
-//	 given().header("Content-Type","application/json").body(pbody).
-//	 when().post("api/users").
-//	 then().log().all().assertThat()
-//	 //.statusCode(200).
-//	 .body("first_name",equalTo("George"));
-////		given().when().get("http://demo.guru99.com/V4/sinkministatement.php?CUSTOMER_ID=68195&PASSWORD=1234!&Account_No=1").then().log()
-////		  .all();
-////		 given().queryParam("CUSTOMER_ID","68195")
-////         .queryParam("PASSWORD","1234!")
-////         .queryParam("Account_No","1")
-////         .when().get("http://demo.guru99.com/V4/sinkministatement.php").then().log()
-////         .body();
-//		
-//	}
+
+		RestAssured.baseURI="https://reqres.in/api/users";
+		RequestSpecification httpRequest = RestAssured.given(); 
+		Response response = httpRequest.get();
+		System.out.println("Status received => " + response.getStatusLine()); 
+		System.out.println("Response=>" + response.prettyPrint());
+		 
+		
+	}
 	public void getmethodPost()
 	{
-		JSONObject jsObj = new JSONObject();
-		jsObj.put("id","50");
-		jsObj.put("email","swethana.v@reqres.in");
-		jsObj.put("first_name", "swetha");
-		jsObj.put("last_name","v");
-		jsObj.put("avatar","https://reqres.in/img/faces/8-image.jpg");
-		 given().header("Content-Type","application/json").body(jsObj.toJSONString()).
-		 when().post("https://reqres.in/api/users").
-		 then().log().all().assertThat()
-		 //.statusCode(201).
-		 .body("first_name",equalTo("swetha"));
+//		JSONObject jsObj = new JSONObject();
+//		jsObj.put("id","50");
+//		jsObj.put("email","swethana.v@reqres.in");
+//		jsObj.put("first_name", "swetha");
+//		jsObj.put("last_name","v");
+//		jsObj.put("avatar","https://reqres.in/img/faces/8-image.jpg");
+//		 given().header("Content-Type","application/json").body(jsObj.toJSONString()).
+//		 when().post("https://reqres.in/api/users").
+//		 then().log().all().assertThat()
+//		 //.statusCode(201).
+//		 .body("first_name",equalTo("swetha"));
 		
 		
 	}
