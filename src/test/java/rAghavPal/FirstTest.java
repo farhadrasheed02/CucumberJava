3e8a70302e2590f3d330d2d29866c042151b865d
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-09 00:22:24 +0530

    Commit By FR

--- src/test/java/rAghavPal/FirstTest.java
+++ src/test/java/rAghavPal/FirstTest.java
@@ -1,16 +1,17 @@
 package rAghavPal;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
 import io.restassured.response.Response;
+import static org.hamcrest.CoreMatchers.*;
 @Test
 public class FirstTest {
 	@Test
-	public void Testing()
+	public void Testing1()
 	{
 		
 		// we read the data from the API using get() --> request...GET: This Verb is used to get or read data from a server.
-		Response response=RestAssured.get("https://reqres.in/api/users?page=2");
+		Response response=get("https://reqres.in/api/users?page=2");
 		System.out.println(response.getBody().asString());
 		//System.out.println(response.statusCode());
 		System.out.println(response.getStatusCode());
@@ -22,5 +23,13 @@ public class FirstTest {
 		String con_type = response.getContentType();
 		Assert.assertEquals(statuscode,200);
 		Assert.assertEquals(con_type,"application/json; charset=utf-8");
+	}
+	@Test
+	public void test2()
+	{
+		baseURI ="https://reqres.in/api";
+		given().get("users?page=2").then().statusCode(200)
+		.body("data[1].id",equalTo(8))  // on this line was getting 
+		.log().all();
 	}
 }
