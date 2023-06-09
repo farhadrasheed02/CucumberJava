f1893dc3d170506dd0a53e9c0b36a2729c94c28c
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-09 22:23:16 +0530

    Commit By FR

--- src/test/java/rAghavPal/PostGet.java
+++ src/test/java/rAghavPal/PostGet.java
@@ -7,15 +7,20 @@ import static org.hamcrest.CoreMatchers.*;
 @Test
 public class PostGet {
 	@Test
-	public void getreq()
+	public void getreq()   // this request is used to read data from the api(https://reqres.in/api/users?page=2)
 	{
-		baseURI="https://reqres.in/api";
+		baseURI="https://reqres.in/";
 		given().
-		get("/users?page=2").
+		get("api/users?page=2").
 		then().
 		statusCode(200).
 		log().all().
 		body("data[4].first_name",equalTo("George"));
+		
+	}
+	
+	public void postreq()
+	{
 		
 	}
 
