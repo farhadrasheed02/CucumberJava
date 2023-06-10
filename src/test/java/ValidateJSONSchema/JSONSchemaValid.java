54d9d55d39e7eb79f3386f59e84b159e13935bba
Farhad Rashid Reshi <farhadrasheed02@gmail.com>
2023-06-10 17:00:44 +0530

    Commit By FR

--- src/test/java/ValidateJSONSchema/JSONSchemaValid.java
+++ src/test/java/ValidateJSONSchema/JSONSchemaValid.java
@@ -3,7 +3,7 @@ package ValidateJSONSchema;
 public class JSONSchemaValid {
 	
 	public void valid() {
-		System.out.println("Validate the schema...");
+		System.out.println("Validate the schema and browse...");
 	}
 
 }
